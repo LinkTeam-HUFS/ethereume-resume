7bdaf670523f7a126a8276861e20f4ecaf643090
Lee Ji Seok <76767226+dev-jiseok@users.noreply.github.com>
2022-05-20 22:02:54 +0900

    22:00

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import JobSeekerPage from '../views/JobSeekerPage'
 import EmployerPage from '../views/EmployerPage'
+import DidPage from '../views/DidPage.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/EmployerPage',
     name: 'EmployerPage',
     component: EmployerPage
+  },
+  {
+    path: '/DidPage',
+    name: 'DidPage',
+    component: DidPage
   }
 ]
 
